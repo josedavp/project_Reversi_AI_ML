7c1ebe74e309473e5e82d1ecc469d72df00cc0c7
Jose Pague <josedavidpague@gmail.com>
2024-04-19 23:55:26 -0500

    Initial reversi files

--- reversi_model.py
+++ reversi_model.py
@@ -13,49 +13,37 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.distributions import Categorical
 import numpy as np
 
-# env = gym.make('CartPole-v1')
-# observation, info = env.reset(seed=42)
+env = gym.make('CartPole-v1')
+observation, info = env.reset(seed=42)
 
-# action = env.action_space.sample()  # this is where you would insert your policy
-# observation, reward, terminated, truncated, info = env.step(action)
+action = env.action_space.sample()  # this is where you would insert your policy
+observation, reward, terminated, truncated, info = env.step(action)
 
-# BATCH_SIZE = 512
-# GAMMA = 0.99
-# EPS_START = 0.9
-# EPS_END = 0.01 #changed from 0.05
-# EPS_DECAY = 500 #1000
-# TAU = 0.005
-# LR = 2e-4 #1e-4
+BATCH_SIZE = 512
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.01 #changed from 0.05
+EPS_DECAY = 500 #1000
+TAU = 0.005
+LR = 2e-4 #1e-4
 
-# # Get number of actions from gym action space
-# n_actions = env.action_space.n
-# # Get the number of state observations
-# state, info = env.reset()
-# n_observations = len(state)
+# Get number of actions from gym action space
+n_actions = env.action_space.n
+# Get the number of state observations
+state, info = env.reset()
+n_observations = len(state)
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# # device = torch.device('cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# device = torch.device('cpu')
 
-# Transition = namedtuple('Transition',
-#                         ('state', 'action', 'next_state', 'reward'))
-
-
-
-
-
-
-
-
-
-
-
-
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
 class ReversiModel:
+    def __init__(self):
+        pass
     
-    
-    
-    def predict():
+    def predict(self):
         pass
 
 class CurrentBoardEnviroment:
@@ -71,25 +59,31 @@ class WeightedBoardEnviroment:
     def getState(self):
         pass
         
-class PolicyNet:
+class PolicyNet(nn.Module): #Actor
+    def __init__(self):
+        pass 
+    
+    def forward(self):
+        pass
+class ValueNet(nn.Module): #Critic
     def __init__(self):
         pass 
     
     def forward(self):
         pass
 
-class ReplayMemory(self):
-    def __init__(self):
-        pass
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
         
-    def push(self):
-        pass
+    def push(self, *args):
+        self.memory.append(Transition(*args))
     
-    def sample(self):
-        pass
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
         
     def __len__(self):
-        pass
+        return len(self.memory)
         
         
 def train():
