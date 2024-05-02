7d070476532a8a68d611cae1be6f7b015ae7d1b6
Jose Pague <josedavidpague@gmail.com>
2024-05-01 21:15:36 -0500

    Template

--- reversi_model copy 2.py
+++ reversi_model copy 2.py
@@ -322,7 +322,31 @@ observation, info = TestEnv.reset(TestEnv.game.board) # seed=42)
 
 
 #########TRYING TO SAVE CHECKPOINT ############
-torch.save()
+# net = DQN()
+# # Additional information
+# EPOCH = 5
+# PATH = "model.pt"
+# LOSS = 0.4
+
+# torch.save({
+#             'epoch': EPOCH,
+#             'model_state_dict': net.state_dict(),
+#             'optimizer_state_dict': optimizer.state_dict(),
+#             'loss': LOSS,
+#             }, PATH)
+
+# model = Net()
+# optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# checkpoint = torch.load(PATH)
+# model.load_state_dict(checkpoint['model_state_dict'])
+# optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+# epoch = checkpoint['epoch']
+# loss = checkpoint['loss']
+
+# model.eval()
+# # - or -
+# model.train()
 ####################
 
 
